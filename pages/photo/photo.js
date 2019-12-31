09c68a45aa2cfd67b2bb2c7c4fd33d35cd92169e
xujin <472829302@qq.com>
2019-12-31 16:45:53 +0800

    2019-12-31

--- pages/photo/photo.js
+++ pages/photo/photo.js
@@ -1,92 +1,183 @@
 // pages/photo/photo.js
 Page({
   beforeRead(event) {
-    const { file, callback } = event.detail;
+    const {
+      file,
+      callback
+    } = event.detail;
     callback(file.type === 'image');
   },
   afterRead(event) {
-    const { file } = event.detail;
+    const {
+      file
+    } = event.detail;
     // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
     wx.uploadFile({
       url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
       filePath: file.path,
       name: 'file',
-      formData: { user: 'test' },
+      formData: {
+        user: 'test'
+      },
       success(res) {
         // 上传完成需要更新 fileList
-        const { fileList = [] } = this.data;
-        fileList.push({ ...file, url: res.data });
-        this.setData({ fileList });
+        const {
+          fileList = []
+        } = this.data;
+        fileList.push({ ...file,
+          url: res.data
+        });
+        this.setData({
+          fileList
+        });
       }
     })
+  },
+  preview(event) {
+    const that = this;
+    let currentUrl = event.currentTarget.dataset.src;
+    console.log(currentUrl)
+    wx.previewImage({
+      current: currentUrl, // 当前显示图片的http链接
+      urls: that.data.img // 需要预览的图片http链接列表
+    })
+  },
+  onClickLeft() {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  onClickRight() {
+    const that = this;
+    wx.chooseImage({
+      count: 8,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success(res) {
+        wx.showLoading({
+          title: '上传中',
+        });
+        let tempFilePaths = res.tempFilePaths;
+        wx.getStorage({
+          key: 'token',
+          success: function(res) {
+            wx.uploadFile({
+              url: 'http://172.24.112.176:8080/upload', //仅为示例，非真实的接口地址
+              header: {
+                'Authorization': 'Bearer' + res.data,
+                'content-type': 'multipart/form-data',
+              },
+              filePath: tempFilePaths,
+              name: 'filePath',
+              formData: {
+                token: res.data   
+              },
+              success(res) {
+                wx.hideLoading();
+                if (res.data.code === 200) {
+                  Notify({ type: 'success', message: res.data.message });
+                  console.log(res)
+                }
+                else {
+                  Notify({ type: 'danger', message: res.data.message });
+                  console.log(res)
+                }
+                that.onLoad();
+              }
+            })
+          },
+        })
+        
+      }
+    })
+    
   },
   /**
    * 页面的初始数据
    */
   data: {
-    fileList: [{ url: 'https://img.yzcdn.cn/vant/leaf.jpg', name: '图片1' },
-      // Uploader 根据文件后缀来判断是否为图片文件
-      // 如果图片 URL 中不包含类型信息，可以添加 isImage 标记来声明
-      {
-        url: 'http://iph.href.lu/60x60?text=default',
-        name: '图片2',
-        isImage: true
-      }]
+    tempFilePaths:"",
+    img: ['D:/0.png', 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg', 'D:/0.png'],
+    array: [{
+      mode: 'scaleToFill',
+      text: 'scaleToFill',
+      src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }, {
+      mode: 'aspectFit',
+      text: 'aspectFit',
+        src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }, {
+      mode: 'aspectFill',
+      text: 'aspectFill',
+      src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }, {
+      mode: 'top',
+      text: 'top',
+      src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }, {
+      mode: 'bottom',
+      text: 'bottom',
+      src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }, {
+      mode: 'center',
+      text: 'center',
+      src: 'https://res.wx.qq.com/wxdoc/dist/assets/img/0.4cb08bb4.jpg'
+    }],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
